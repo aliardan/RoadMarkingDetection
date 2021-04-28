d4a4662025ef55c156b9ea3404e2c3ff043951fd
aliardan <aliardanchik@gmail.com>
2021-04-28 22:41:38 +0300

    Scorer implementation

---
+++ Scorer/Models/Abstract/YoloModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Scorer.YoloParser;
+
+
+namespace Scorer.Models.Abstract
+{
+    /// <summary>
+    /// Model descriptor.
+    /// </summary>
+    public abstract class YoloModel
+    {
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+        public abstract int Depth { get; }
+
+        public abstract int Dimensions { get; }
+
+        public abstract float Confidence { get; }
+        public abstract float MulConfidence { get; }
+        public abstract float Overlap { get; }
+
+        public abstract string Weights { get; }
+
+        public List<YoloLabel> Labels { get; protected set; }
+
+        public bool Ready { get; set; }
+    }
+}
