b98170eec548180d4102d8db849300840fa8648e
aliardan <aliardanchik@gmail.com>
2021-04-29 00:06:20 +0300

    Example assets

--- RoadMarkingDetection/Program.cs
+++ RoadMarkingDetection/Program.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using Scorer.Models;
-using Microsoft.ML;
 using Scorer.DataStructures;
 using Scorer.YoloParser;
 
@@ -13,56 +13,53 @@ namespace RoadMarkingDetection
     {
         static void Main(string[] args)
         {
-            var assetsRelativePath = @"../../../assets";
+            var assetsRelativePath = @"..\\..\\..\\Assets";
             string assetsPath = GetAbsolutePath(assetsRelativePath);
-            var modelFilePath = Path.Combine(assetsPath, "Model", "yolov5s.onnx");
-            var imagesFolder = Path.Combine(assetsPath, "images");
-            var outputFolder = Path.Combine(assetsPath, "images", "output");
-
-            // Initialize MLContext
-            MLContext mlContext = new MLContext();
+            var imagesFolder = Path.Combine(assetsPath, "input");
 
             // Load Data
             IEnumerable<ImageNetData> images = ImageNetData.ReadFromFile(imagesFolder);
-            IDataView imageDataView = mlContext.Data.LoadFromEnumerable(images);
+            
+            Console.WriteLine("=========Identify the objects in the images=========");
+            Console.WriteLine("====================================================");
 
-            var scorer = new YoloScorer<YoloCocoModel>();
-
-            using var stream = new FileStream("assets/roadmark_0620.jpg", FileMode.Open);
-
-            var image = Image.FromStream(stream);
-
-            List<YoloPrediction> predictions = scorer.Predict(image);
-
-            using var graphics = Graphics.FromImage(image);
-
-            Console.WriteLine("=====Identify the objects in the images=====");
-            Console.WriteLine("");
-
-            foreach (var prediction in predictions) // iterate each prediction to draw results
+            for (var i = 1; i <= images.Count(); i++)
             {
-                double score = Math.Round(prediction.Score, 2);
+                var scorer = new YoloScorer<YoloCocoModel>(); using var stream = new FileStream($"Assets/input/{i}.jpg", FileMode.Open);
 
-                graphics.DrawRectangles(new Pen(prediction.Label.Color, 2),
-                    new[] { prediction.Rectangle });
+                var image = Image.FromStream(stream);
 
-                var (x, y) = (prediction.Rectangle.X - 2, prediction.Rectangle.Y - 21);
+                List<YoloPrediction> predictions = scorer.Predict(image);
 
-                graphics.DrawString($"{prediction.Label.Name} ({score * 100}%)", new Font("TimesNewRoman", 14),
-                    new SolidBrush(prediction.Label.Color), new PointF(x, y));
-                Console.WriteLine($"{prediction.Label.Name} and its Confidence score: {score * 100}%");
+                using var graphics = Graphics.FromImage(image);
+
+                Console.WriteLine($"=====Identify the objects in the image number {i}=====");
+                Console.WriteLine("");
+
+                foreach (var prediction in predictions) // iterate each prediction to draw results
+                {
+                    double score = Math.Round(prediction.Score, 2);
+
+                    graphics.DrawRectangles(new Pen(prediction.Label.Color, 2),
+                        new[] { prediction.Rectangle });
+
+                    var (x, y) = (prediction.Rectangle.X - 2, prediction.Rectangle.Y - 21);
+
+                    graphics.DrawString($"{prediction.Label.Name} ({score * 100}%)", new Font("TimesNewRoman", 14),
+                        new SolidBrush(prediction.Label.Color), new PointF(x, y));
+                    Console.WriteLine($"{prediction.Label.Name} and its Confidence score: {score * 100}%");
+                    image.Save($"Assets/output/result{i}.jpg");
+                }
+                Console.WriteLine("");
             }
-
-            image.Save("assets/result.jpg");
-            Console.WriteLine("");
-            Console.WriteLine("=========End of Process..Hit any Key========");
+            
+            Console.WriteLine("=============End of Process..Hit any Key============");
         }
 
         public static string GetAbsolutePath(string relativePath)
         {
             FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
             string assemblyFolderPath = _dataRoot.Directory.FullName;
-
             string fullPath = Path.Combine(assemblyFolderPath, relativePath);
 
             return fullPath;
